493d711dc8b16f37ba298031f163722b70d2842d
Мария Шабалина <masha290208@gmail.com>
2024-11-17 12:51:15 +0500

    17.11.24

--- main.cpp
+++ main.cpp
@@ -1,8 +1,89 @@
 #include <iostream>
+#include <cstdlib>
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	/*int magicNumber{}, userNumber{}, count{}, live = 25, choose{};
+	std::cout << "Игра угадай число!\n\n";
+	while (true)
+	{
+		std::cout << "1 - Легкий уровень, 2 - Сложный, 0 - Выход\n";
+		std::cout << "Ввод: ";
+		std::cin >> choose;
+		if (choose == 0)
+		{
+			break;
+		}
+		else if (choose == 1)
+		{
+			magicNumber = rand() % 10 + 1;
+			std::cout << "\nУгадайте число от 1 до 10\n";
+			while (true)
+			{
+				std::cout << "Введите число: ";
+				std::cin >> userNumber;
+				count++;
+				if (userNumber == magicNumber)
+				{
+					std::cout << "Вы угадали! Колличество попыток: " << count << "\n\n";
+					count = 0;
+					break;
+				}
+				std::cout << "Не верно\n";
+			}			
+		}
+		else if (choose == 2)
+		{
+			magicNumber = rand() % 500 + 1;
+			std::cout << "\nУгадайте число от 1 до 500\n";
+			while (true)
+			{
+				std::cout << "\nЖизней: " << live << "\n";
+				std::cout << "Введите число: ";
+				std::cin >> userNumber;			
+				if (userNumber == magicNumber)
+				{
+					std::cout << "Вы угадали! Колличество оставшихся жизней: " << live << "\n\n";					
+					break;
+				}
+				std::cout << "Не верно\n";
+				live--;
+				if (live <= 0)
+				{
+					std::cout << "Игра окончена\n" << "Число было: " << magicNumber << "\n\n";
+					live = 25;
+					break;
+				}
+				std::cout << "\nЖизней: " << live << "\n";
+				std::cout << "1 - Взять подсказку (стоимость 1 жизнь), 2 - Пропустить\n" << "Ввод: ";
+				std::cin >> choose;
+				if (choose == 1)
+				{
+					live--;
+					if (live <= 0)
+					{
+						std::cout << "Игра окончена\n" << "Число было: " << magicNumber << "\n\n";
+						live = 25;
+						break;
+					}
+					if (userNumber < magicNumber)
+					{
+						std::cout << "Ваше число меньше числа компьютера\n";
+					}
+					else
+					{
+						std::cout << "Ваше число больше числа компьютера\n";
+					}
+				}
+			}
+		}
+		else
+		{
+			std::cout << "Некорректный ввод\n";
+		}
+	}*/
 	/*float ab{}, bc{}, ves{}, k{}, bak = 300;
 	std::cout << "Введите расстояние от A до B(км): ";
 	std::cin >> ab;
